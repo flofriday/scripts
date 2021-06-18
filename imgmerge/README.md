344c8d3ce60df8ee8c943f12ffb7bf6660b918c1
flofriday <flohacksfriday@gmail.com>
2021-06-18 15:58:10 +0200

    Add a image merging tool

---
+++ imgmerge/README.md
@@ -0,0 +1,15 @@
+#imgmerge
+A super simple script to merge 2 or more images side by side.
+
+````
+usage: imgmerge.py [-h] [-v] [-o] file [file ...]
+
+positional arguments:
+  file             Image files to merge
+
+optional arguments:
+  -h, --help       show this help message and exit
+  -v, --vertical   Merge the images vertically
+  -o, --overwrite  Overwrite the first image and delete all others
+```
+
---
+++ imgmerge/imgmerge.py
@@ -0,0 +1,68 @@
+import argparse
+import sys
+import os
+from PIL import Image
+from functools import reduce
+
+
+def main():
+    # Parse the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", nargs="+", help="Image files to merge")
+    parser.add_argument(
+        "-v",
+        "--vertical",
+        help="Merge the images vertically",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        help="Overwrite the first image and delete all others",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    if len(args.file) == 1:
+        print(
+            "Cannot merge an image with itself. Provide at least two images."
+        )
+        sys.exit(1)
+
+    # Open the input files
+    filenames = args.file
+    images = list(map(lambda f: Image.open(f), filenames))
+
+    # Merge the images
+    if args.vertical:
+        width = reduce(lambda a, b: max(a.width, b.width), images)
+        height = reduce(lambda a, b: a.height + b.height, images)
+    else:
+        width = reduce(lambda a, b: a.width + b.width, images)
+        height = reduce(lambda a, b: max(a.height, b.height), images)
+
+    result = Image.new("RGBA", (width, height), color="#000000FF")
+
+    x, y = 0, 0
+    for img in images:
+        result.paste(img, (x, y))
+
+        if not args.vertical:
+            x += img.width
+        else:
+            y += img.height
+
+        img.close()
+
+    # Save the image
+    if args.overwrite:
+        for file in filenames[1:]:
+            os.remove(file)
+        result.save(filenames[0])
+
+    else:
+        result.save("out.png")
+
+
+if __name__ == "__main__":
+    main()
